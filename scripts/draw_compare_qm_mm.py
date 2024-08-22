ad00b01a6d74ccfc49926d63601aec437eda5bfc
snljty <snljty@gmail.com>
2024-08-22 16:31:02 +0800

    fix conflict

--- scripts/draw_compare_qm_mm.py
+++ scripts/draw_compare_qm_mm.py
@@ -1,22 +1,11 @@
 #! /usr/bin/env python3
 # -*- Coding: UTF-8 -*-
 
-import os
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 plt.rc("font", size = 14)
-
-if os.path.isfile("x.txt"):
-    x = np.loadtxt("x.txt")
-else:
-    start_value = -1.10287 # the start dihedral
-    num_scan = 36
-    step_value = 10.0
-    x = np.arange(num_scan + 1).astype(np.float64) * step_value + start_value
-    x = (x + 180.0) % 360 - 180.0
-
-num_dims = 3
 
 # auxiliary
 def Get_vector_angle(p: np.double, q: np.double) -> np.double:
@@ -48,7 +37,9 @@ def Get_dihedral(a: np.double, b: np.double, c: np.double, d: np.double) -> np.d
     q = np.cross(c - d, b - c)
     return Get_vector_angle(p, q) * (-1.0 if np.dot(p, c - d) < 0.0 else 1.0)
 
-if os.path.isfile("traj.xyz"):
+if os.path.isfile("x.txt"):
+    x = np.loadtxt("x.txt")
+elif os.path.isfile("traj.xyz"):
     if os.path.isfile("dihedral.txt"):
         with open("dihedral.txt") as ifl:
             atom1, atom2, atom3, atom4 = map(int, ifl.readline().split())
@@ -71,37 +62,46 @@ if os.path.isfile("traj.xyz"):
                         line.insert(0, "D")
                     assert line[5] == "S"
                     atom1, atom2, atom3, atom4 = map(int, line[1:5])
-if "atom1" in dir():
-    num_scan = -1
-    with open("traj.xyz") as ifl:
-        num_atoms = int(ifl.readline())
-    element_names = ["" for _ in range(num_atoms)]
-    coordinates = []
-    x = []
-    with open("traj.xyz") as ifl:
-        while True:
-            line = ifl.readline()
-            if not line: break
-            assert int(line) == num_atoms
-            ifl.readline()
-            num_scan += 1
-            coordinates.append(np.zeros((num_atoms, num_dims), dtype = np.double))
-            for i in range(num_atoms):
-                line = ifl.readline().split()
-                if element_names[i]:
-                    assert line[0] == element_names[i]
-                else:
-                    element_names[i] = line[0]
-                coordinates[num_scan][i, :] = np.array(line[1:], dtype = np.double)
-            x.append(Get_dihedral(coordinates[num_scan][atom1 - 1, :], \
-                                  coordinates[num_scan][atom2 - 1, :], \
-                                  coordinates[num_scan][atom3 - 1, :], \
-                                  coordinates[num_scan][atom4 - 1, :]))
-    x = np.array(x, dtype = np.double)
-else:
-    print("Note: using dihedrals set in this script ({:s}).".format(sys.argv[0]))
+if "x" not in dir():
+    if "atom1" in dir():
+        num_scan = -1
+        with open("traj.xyz") as ifl:
+            num_atoms = int(ifl.readline())
+        element_names = ["" for _ in range(num_atoms)]
+        coordinates = []
+        x = []
+        with open("traj.xyz") as ifl:
+            while True:
+                line = ifl.readline()
+                if not line: break
+                assert int(line) == num_atoms
+                ifl.readline()
+                num_scan += 1
+                coordinates.append(np.zeros((num_atoms, num_dims), dtype = np.double))
+                for i in range(num_atoms):
+                    line = ifl.readline().split()
+                    if element_names[i]:
+                        assert line[0] == element_names[i]
+                    else:
+                        element_names[i] = line[0]
+                    coordinates[num_scan][i, :] = np.array(line[1:], dtype = np.double)
+                x.append(Get_dihedral(coordinates[num_scan][atom1 - 1, :], \
+                                      coordinates[num_scan][atom2 - 1, :], \
+                                      coordinates[num_scan][atom3 - 1, :], \
+                                      coordinates[num_scan][atom4 - 1, :]))
+        x = np.array(x, dtype = np.double)
+    else:
+        print("Note: using dihedrals set in this script ({:s}).".format(sys.argv[0]))
+        start_value = -1.10287 # the start dihedral
+        num_scan = 36
+        step_value = 10.0
+        x = np.arange(num_scan + 1).astype(np.float64) * step_value + start_value
+        x = (x + 180.0) % 360 - 180.0
+
+num_dims = 3
 
 title = "structure"
+print(f"title = {title:s}")
 x_label = u"dihedral (\u00b0)"
 
 y_qm = np.loadtxt("qm_scan_energy.txt", unpack = True)
